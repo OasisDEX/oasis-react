8ec49f8f79e12b9c3287b2dcd270e1bdb03d506c
John Doe <georgi.h.georgieff@gmail.com>
2017-12-05 12:43:45 +0100

    Styling of the GET STARTED and WARNING message boards. (#17)

--- src/components/OasisMessage.jsx
+++ src/components/OasisMessage.jsx
@@ -1,26 +1,34 @@
 import React, { PureComponent } from 'react';
 import { PropTypes } from 'prop-types';
-// import ImmutablePropTypes from 'react-immutable-proptypes';
 
 import styles from './OasisMessage.scss';
+import CSSModules from 'react-css-modules';
 
 export const MSGTYPE_INFO = 'MSGTYPE_INFO';
 export const MSGTYPE_WARNING = 'MSGTYPE_WARNING';
-export const MSGTYPE_ERROR = 'MSGTYPE_ERROR';
+
+const BOARD_TYPE = {
+  [MSGTYPE_INFO]: "MessageBoard--Info",
+  [MSGTYPE_WARNING]: "MessageBoard--Warning",
+};
 
 const propTypes = PropTypes && {
   children: PropTypes.node.isRequired,
   heading: PropTypes.string.isRequired,
-  type: PropTypes.oneOf([MSGTYPE_INFO, MSGTYPE_WARNING, MSGTYPE_ERROR]),
+  type: PropTypes.oneOf([MSGTYPE_INFO, MSGTYPE_WARNING]),
 };
 const defaultProps = {};
 
 class OasisMessage extends PureComponent {
   render() {
     return (
-      <div className={styles.base}>
-        <h3>{this.props.heading}</h3>
-        <div className={styles.OasisMessageBody}>
+      <div styleName={`MessageBoard ${BOARD_TYPE[this.props.type]}`}>
+        <h3 styleName="Heading">
+          {this.props.heading}
+
+          <span styleName="Close"/>
+        </h3>
+        <div>
           {this.props.children}
         </div>
       </div>
@@ -31,4 +39,4 @@ class OasisMessage extends PureComponent {
 OasisMessage.displayName = 'OasisMessage';
 OasisMessage.propTypes = propTypes;
 OasisMessage.defaultProps = defaultProps;
-export default OasisMessage;
+export default CSSModules(OasisMessage, styles, {allowMultiple: true});
