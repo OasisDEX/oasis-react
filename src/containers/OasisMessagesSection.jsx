97e9d15473aa4a2c6eda8b7ac183ecc6c44f6841
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-12-05 12:37:50 +0100

    Styling of the GET STARTED and WARNING message boards.

--- src/containers/OasisMessagesSection.jsx
+++ src/containers/OasisMessagesSection.jsx
@@ -5,6 +5,10 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import OasisMessage from '../components/OasisMessage';
+import { MSGTYPE_INFO, MSGTYPE_WARNING } from '../components/OasisMessage';
+
+import styles from '../components/OasisMessage.scss';
+import CSSModule from 'react-css-modules';
 
 const propTypes = PropTypes && {
   actions: PropTypes.object,
@@ -13,28 +17,53 @@ const propTypes = PropTypes && {
 export class OasisMessagesSectionWrapper extends PureComponent {
   render() {
     return (
-      <div>
-        <OasisMessage heading={'Get started!'}>
-          <ul>
-            <li>
-              Go to wrap/unwrap and wrap ETH to turn it into W-ETH.
-            </li>
-            <li>
-              Go to Trade and place new orders or trade existing orders by
-              clicking on an open order on the order book to buy/sell into it.
-            </li>
-            <li>
-              The first time you make a transaction you will be prompted to
-              set your allowance.
-            </li>
-          </ul>
-        </OasisMessage>
-        <OasisMessage heading={'Warning!'}>
-          Oasis is undergoing alpha testing: Any funds deposited on the exchange could be lost in the event of a
-          security breach.
-        </OasisMessage>
+      <div className="row">
+        <div className="col-md-7">
+          <OasisMessage type={MSGTYPE_INFO} heading={'Get started!'}>
+            <ul>
+              <li>
+                <span styleName="Circle">
+                  <span styleName="Number">1</span>
+                </span>
+                <span styleName="Text">
+                  Go to wrap/unwrap and wrap ETH to turn it into W-ETH.
+                </span>
+              </li>
+              <li>
+                <span styleName="Circle">
+                  <span styleName="Number">2</span>
+                </span>
+                <span styleName="Text">
+                  Go to Trade and place new orders or trade existing orders by clicking on an open order on the order book to buy/sell into it.
+                </span>
+              </li>
+              <li>
+                <span styleName="Circle">
+                  <span styleName="Number">3</span>
+                </span>
+                <span styleName="Text">
+                  The first time you make a transaction you will be prompted to set your allowance.
+                </span>
+              </li>
+            </ul>
+          </OasisMessage>
+        </div>
+        <div className="col-md-5">
+          <OasisMessage type={MSGTYPE_WARNING} heading={'Warning!'}>
+            <ul>
+              <li>
+                <span styleName="Circle Circle--Red">
+                 <span styleName="FilledCircle"/>
+                </span>
+                <span styleName="Text">
+                  Oasis is undergoing alpha testing: Any funds deposited on the exchange could be lost in the event of a
+                  security breach.
+                </span>
+              </li>
+            </ul>
+          </OasisMessage>
+        </div>
       </div>
-
     );
   }
 }
@@ -45,9 +74,9 @@ export function mapStateToProps(state) {
 
 export function mapDispatchToProps(dispatch) {
   const actions = {};
-  return { actions: bindActionCreators(actions, dispatch) };
+  return {actions: bindActionCreators(actions, dispatch)};
 }
 
 OasisMessagesSectionWrapper.propTypes = propTypes;
 OasisMessagesSectionWrapper.displayName = 'OasisMessagesSection';
-export default connect(mapStateToProps, mapDispatchToProps)(OasisMessagesSectionWrapper);
+export default connect(mapStateToProps, mapDispatchToProps)(CSSModule(OasisMessagesSectionWrapper, styles, { allowMultiple: true }));
