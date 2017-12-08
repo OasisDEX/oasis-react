25b300d7ac47b0e555131c5ec7896322a76aa160
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-12-08 18:47:13 +0100

    Locked account page design

--- src/components/Locked.jsx
+++ src/components/Locked.jsx
@@ -1,21 +1,30 @@
 import React, { PureComponent } from 'react';
 import { PropTypes } from 'prop-types';
-// import ImmutablePropTypes from 'react-immutable-proptypes';
+import MetamaskLogo from '../assets/dapphub_icn_metamask.svg';
 
 import styles from './Locked.scss';
+import CSSModules from 'react-css-modules';
 
 
-const propTypes = PropTypes && {
-};
+const propTypes = PropTypes && {};
 const defaultProps = {};
 
 
 class Locked extends PureComponent {
   render() {
     return (
-      <div className={styles.base}>
-        Account locked
-      </div>
+      <section styleName="LockedAccountSection">
+        <img styleName="MetamaskLogo" alt="Metamask Fox Logo" src={MetamaskLogo}/>
+        <hr styleName="Separator"/>
+        <h2 styleName="Heading">METAMASK ACCOUNT LOCKED</h2>
+        <p styleName="Message">
+          You are trying to access OasisDEX without an unlocked account.<br/>
+          Unlock your account on the Metamask Extension.
+        </p>
+        <hr styleName="Separator"/>
+
+      </section>
+    //  FOOTER PLACEHOLDER
     );
   }
 }
@@ -23,4 +32,4 @@ class Locked extends PureComponent {
 Locked.displayName = 'Locked';
 Locked.propTypes = propTypes;
 Locked.defaultProps = defaultProps;
-export default Locked;
+export default CSSModules(Locked, styles);
