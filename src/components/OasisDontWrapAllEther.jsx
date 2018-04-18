10b5d2faea7e61aea85bcbf38bc4c45d33654ef7
Aneta Rosłan <aneta.roslan@in4mates.com>
2018-04-18 15:48:42 +0200

    Styling - Wrap box

--- src/components/OasisDontWrapAllEther.jsx
+++ src/components/OasisDontWrapAllEther.jsx
@@ -3,6 +3,8 @@ import { PropTypes } from 'prop-types';
 // import ImmutablePropTypes from 'react-immutable-proptypes';
 
 import styles from './OasisDontWrapAllEther.scss';
+import {InfoBox} from "./InfoBox";
+import {InfoBoxBody} from "./InfoBoxBody";
 
 
 const propTypes = PropTypes && {
@@ -13,9 +15,11 @@ const defaultProps = {};
 class OasisDontWrapAllEther extends PureComponent {
   render() {
     return (
-      <div className={styles.base}>
+      <InfoBox color='danger' className={styles.base}>
+        <InfoBoxBody>
         Do not wrap all of your ETH! Otherwise you cannot pay for transactions.
-      </div>
+        </InfoBoxBody>
+      </InfoBox>
     );
   }
 }
