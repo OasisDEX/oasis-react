23641d79eec2219dd2107b1d3250a70f24622d6c
Arkadiusz S <comfort.of.logic@googlemail.com>
2018-05-06 18:53:07 +0200

    Fix my orders view & Gas estimate error indicator (#127)
    * Fix gas estimate error indicator.
    
    * Fixed my orders view.

--- src/components/OasisMyOrders.jsx
+++ src/components/OasisMyOrders.jsx
@@ -1,58 +1,55 @@
-import React, { PureComponent } from 'react';
-import { PropTypes } from 'prop-types';
-import BigNumber from  'bignumber.js';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import moment from 'moment';
+import React, { PureComponent } from "react";
+import { PropTypes } from "prop-types";
+import BigNumber from "bignumber.js";
+import ImmutablePropTypes from "react-immutable-proptypes";
+import moment from "moment";
 
-import { DESCENDING, orderByTimestamp } from '../utils/sort';
-import { formatAmount, formatPrice, price } from '../utils/tokens/pair';
-import { OasisTable } from './OasisTable';
-import { OasisTradeType } from './OasisTradeType';
-import OasisWidgetFrame from '../containers/OasisWidgetFrame';
-import web3 from '../bootstrap/web3';
-import { ASK, BID } from '../store/reducers/trades';
-import { ETH_UNIT_ETHER } from '../constants';
-import { isOfferOwner } from '../utils/orders';
+import { DESCENDING, orderByTimestamp } from "../utils/sort";
+import { formatAmount, formatPrice, price } from "../utils/tokens/pair";
+import { OasisTable } from "./OasisTable";
+import { OasisTradeType } from "./OasisTradeType";
+import OasisWidgetFrame from "../containers/OasisWidgetFrame";
+import web3 from "../bootstrap/web3";
+import { ASK, BID } from "../store/reducers/trades";
+import { ETH_UNIT_ETHER } from "../constants";
+import { isOfferOwner } from "../utils/orders";
 import OasisSelect from "./OasisSelect";
-import styles from './OasisMyOrders.scss';
+import styles from "./OasisMyOrders.scss";
 import OasisButton from "./OasisButton";
 
-
-
 const myOrdersDisplayFormat = offer => {
-
-  let baseAmount = null, quoteAmount = null;
+  let baseAmount = null,
+    quoteAmount = null;
   switch (offer.tradeType) {
-    case 'buy':
-      baseAmount = formatAmount(offer.buyHowMuch, true,  ETH_UNIT_ETHER);
+    case BID:
+      baseAmount = formatAmount(offer.buyHowMuch, true, ETH_UNIT_ETHER);
       quoteAmount = formatAmount(offer.sellHowMuch, true, ETH_UNIT_ETHER);
-    break;
-    case 'sell':
-      baseAmount = formatAmount(offer.sellHowMuch, true,  ETH_UNIT_ETHER);
+      break;
+    case ASK:
+      baseAmount = formatAmount(offer.sellHowMuch, true, ETH_UNIT_ETHER);
       quoteAmount = formatAmount(offer.buyHowMuch, true, ETH_UNIT_ETHER);
-    break;
+      break;
   }
 
-  return ({
-    tradeType: offer.tradeType,
+  return {
+    tradeTypeEl: offer.tradeTypeEl,
     price: formatPrice(offer.price),
     baseAmount,
     quoteAmount,
     owner: offer.owner,
     id: offer.id
-  });
+  };
 };
 
-
-const myOpenOffersFilter = (entry) => {
+const myOpenOffersFilter = entry => {
   const myAccountAddress = web3.eth.defaultAccount;
-  return (entry.owner.toString() === myAccountAddress.toString());
+  return entry.owner.toString() === myAccountAddress.toString();
 };
 
-const myOffersFilter = (entry) => {
+const myOffersFilter = entry => {
   const myAccountAddress = web3.eth.defaultAccount;
-  const isOfferMaker = (entry.maker.toString() === myAccountAddress.toString());
-  const isOfferTaker = (entry.taker.toString() === myAccountAddress.toString());
+  const isOfferMaker = entry.maker.toString() === myAccountAddress.toString();
+  const isOfferTaker = entry.taker.toString() === myAccountAddress.toString();
   return isOfferMaker || isOfferTaker;
 };
 
@@ -62,36 +59,32 @@ const actionsColumnTemplate = function(offer) {
     this.cancelOffer(offer);
     cancelPending = true;
   };
-  return (
-    isOfferOwner(offer) ?
-      (
-        <OasisButton
-            disabled={cancelPending}
-            size='xs'
-            onClick={onCancel}
-            className={styles.cancelButton}
-        >
-          {cancelPending ? '...' : 'cancel'}
-        </OasisButton>
-      ) : null
-  );
+  return isOfferOwner(offer) ? (
+    <OasisButton
+      disabled={cancelPending}
+      size="xs"
+      onClick={onCancel}
+      className={styles.cancelButton}
+    >
+      {cancelPending ? "..." : "cancel"}
+    </OasisButton>
+  ) : null;
 };
 
 const tradesHistoryColsDefinition = (baseToken, quoteToken) => [
-    { heading: 'date', key: 'date' },
-    { heading: 'action', key: 'tradeType' },
-    { heading: `price`, key: 'price' },
-    { heading: `${quoteToken}`, key: 'quoteAmount' },
-    { heading: `${baseToken}`, key: 'baseAmount' },
+  { heading: "date", key: "date" },
+  { heading: "action", key: "tradeType" },
+  { heading: `price`, key: "price" },
+  { heading: `${quoteToken}`, key: "quoteAmount" },
+  { heading: `${baseToken}`, key: "baseAmount" }
 ];
 
-
 const openOrdersColsDefinition = (baseToken, quoteToken, orderActions) => [
-  { heading: 'action', key: 'tradeType' },
-  { heading: `price`, key: 'price' },
-  { heading: `${quoteToken}`, key: 'quoteAmount' },
-  { heading: `${baseToken}`, key: 'baseAmount' },
-  { heading: ``, template: actionsColumnTemplate.bind(orderActions)}
+  { heading: "action", key: "tradeTypeEl" },
+  { heading: `price`, key: "price" },
+  { heading: `${quoteToken}`, key: "quoteAmount" },
+  { heading: `${baseToken}`, key: "baseAmount" },
+  { heading: ``, template: actionsColumnTemplate.bind(orderActions) }
 ];
 
 const propTypes = PropTypes && {
@@ -99,13 +92,10 @@ const propTypes = PropTypes && {
 };
 const defaultProps = {};
 
-
-
 class OasisMyOrders extends PureComponent {
-
   constructor(props) {
     super(props);
-    this.viewType = 'Open';
+    this.viewType = "Open";
     this.onViewTypeChange = this.onViewTypeChange.bind(this);
   }
 
@@ -115,17 +105,33 @@ class OasisMyOrders extends PureComponent {
   }
 
   renderOpenOffers() {
-    const { activeTradingPair: { baseToken, quoteToken }, sellOffers = [], buyOffers = [], cancelOffer } = this.props;
+    const {
+      activeTradingPair: { baseToken, quoteToken },
+      sellOffers = [],
+      buyOffers = [],
+      cancelOffer
+    } = this.props;
     const orderActions = { cancelOffer };
-    const myOpenOffers =
-      sellOffers
-        .map(so => ({...so, tradeType: <OasisTradeType type={ASK} />, price: so.ask_price }) )
-        .concat(buyOffers.map(bo => ({...bo, tradeType: <OasisTradeType type={BID} />, price: bo.bid_price }) ))
-        .filter(myOpenOffersFilter)
-        .sort((p, c) => p.bid_price_sort < c.bid_price_sort ? 1 : -1)
-        .map(myOrdersDisplayFormat);
+    const myOpenOffers = sellOffers
+      .map(so => ({
+        ...so,
+        tradeType: ASK,
+        tradeTypeEl: <OasisTradeType type={ASK} />,
+        price: so.ask_price
+      }))
+      .concat(
+        buyOffers.map(bo => ({
+          ...bo,
+          tradeType: BID,
+          tradeTypeEl: <OasisTradeType type={BID} />,
+          price: bo.bid_price
+        }))
+      )
+      .filter(myOpenOffersFilter)
+      .sort((p, c) => (p.bid_price_sort < c.bid_price_sort ? 1 : -1))
+      .map(myOrdersDisplayFormat);
 
-    if(myOpenOffers.count()) {
+    if (myOpenOffers.count()) {
       return (
         <OasisTable
           rows={myOpenOffers.toArray()}
@@ -136,76 +142,87 @@ class OasisMyOrders extends PureComponent {
     } else {
       return (
         <div className={styles.info}>You currently have no active offers</div>
-      )
+      );
     }
   }
 
   renderTradesHistory() {
+    const toHistoricalTrades = tradeHistoryEntry => {
+      let baseAmount = null,
+        quoteAmount = null;
 
-    const toHistoricalTrades = (tradeHistoryEntry) => {
-      let baseAmount = null, quoteAmount = null;
-
-      if (tradeHistoryEntry.buyWhichToken === quoteToken && tradeHistoryEntry.sellWhichToken === baseToken) {
+      if (
+        tradeHistoryEntry.buyWhichToken === quoteToken &&
+        tradeHistoryEntry.sellWhichToken === baseToken
+      ) {
         baseAmount = new BigNumber(tradeHistoryEntry.sellHowMuch);
         quoteAmount = new BigNumber(tradeHistoryEntry.buyHowMuch);
-      } else if (tradeHistoryEntry.buyWhichToken === baseToken && tradeHistoryEntry.sellWhichToken === quoteToken) {
+      } else if (
+        tradeHistoryEntry.buyWhichToken === baseToken &&
+        tradeHistoryEntry.sellWhichToken === quoteToken
+      ) {
         baseAmount = new BigNumber(tradeHistoryEntry.buyHowMuch);
         quoteAmount = new BigNumber(tradeHistoryEntry.sellHowMuch);
       }
 
       return {
-        date: moment.unix(tradeHistoryEntry.timestamp).format('DD-MM-HH:mm'),
-        tradeType: <OasisTradeType order={tradeHistoryEntry} baseCurrency={baseToken}/>,
+        date: moment.unix(tradeHistoryEntry.timestamp).format("DD-MM-HH:mm"),
+        tradeType: (
+          <OasisTradeType order={tradeHistoryEntry} baseCurrency={baseToken} />
+        ),
         baseAmount: formatAmount(baseAmount, true),
         quoteAmount: formatAmount(quoteAmount, true),
-        price: formatPrice(
-          price(tradeHistoryEntry, baseToken, quoteToken),
-        ),
+        price: formatPrice(price(tradeHistoryEntry, baseToken, quoteToken))
       };
     };
     const { trades, activeTradingPair: { baseToken, quoteToken } } = this.props;
     const myTrades = trades.filter(myOffersFilter);
-    const marketHistory = orderByTimestamp(myTrades.toJSON(), DESCENDING).map(toHistoricalTrades);
+    const marketHistory = orderByTimestamp(myTrades.toJSON(), DESCENDING).map(
+      toHistoricalTrades
+    );
     if (myTrades.count()) {
       return (
         <OasisTable
           rows={marketHistory}
           col={tradesHistoryColsDefinition(baseToken, quoteToken)}
-          className={styles.tradesHistory}/>
+          className={styles.tradesHistory}
+        />
       );
     } else {
-      return (<div className={styles.info}>Your trades history is empty</div>);
+      return <div className={styles.info}>Your trades history is empty</div>;
     }
-
   }
 
   renderContent() {
     switch (this.viewType) {
-      case 'Open': return this.renderOpenOffers();
-      case 'Closed': return this.renderTradesHistory();
+      case "Open":
+        return this.renderOpenOffers();
+      case "Closed":
+        return this.renderTradesHistory();
     }
   }
 
-
   render() {
-    var select = <OasisSelect
+    var select = (
+      <OasisSelect
         onChange={this.onViewTypeChange}
         value={this.viewType}
         className={styles.select}
       >
-        <option value={'Open'}>Open</option>
-        <option value={'Closed'}>Closed</option>
-      </OasisSelect>;
+        <option value={"Open"}>Open</option>
+        <option value={"Closed"}>Closed</option>
+      </OasisSelect>
+    );
 
     return (
-      <OasisWidgetFrame heading={'MY ORDERS'} headingChildren={select}>
+      <OasisWidgetFrame heading={"MY ORDERS"} headingChildren={select}>
         <div>{this.renderContent()}</div>
       </OasisWidgetFrame>
     );
   }
 }
 
-OasisMyOrders.displayName = 'OasisMyOrders';
+OasisMyOrders.displayName = "OasisMyOrders";
 OasisMyOrders.propTypes = propTypes;
 OasisMyOrders.defaultProps = defaultProps;
 export default OasisMyOrders;
--- src/containers/OasisGasPrice.jsx
+++ src/containers/OasisGasPrice.jsx
@@ -1,19 +1,20 @@
-import React, { PureComponent } from 'react';
-import { PropTypes } from 'prop-types';
+import React, { PureComponent } from "react";
+import { PropTypes } from "prop-types";
 // import ImmutablePropTypes from 'react-immutable-proptypes';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import transactions from '../store/selectors/transactions';
-import network from '../store/selectors/network';
-import web3 from '../bootstrap/web3';
-import { ETH_UNIT_ETHER } from '../constants';
-import { formatAmount } from '../utils/tokens/pair';
-import {FlexBox} from "../components/FlexBox";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import transactions from "../store/selectors/transactions";
+import network from "../store/selectors/network";
+import web3 from "../bootstrap/web3";
+import { ETH_UNIT_ETHER } from "../constants";
+import { formatAmount } from "../utils/tokens/pair";
+import { FlexBox } from "../components/FlexBox";
 
-import styles from './OasisGasPrice.scss';
-import CSSModules from 'react-css-modules';
-
+import styles from "./OasisGasPrice.scss";
+import CSSModules from "react-css-modules";
+import InfoBox from "../components/InfoBox";
+import InfoBoxBody from "../components/InfoBoxBody";
 
 const propTypes = PropTypes && {
   actions: PropTypes.object.isRequired,
@@ -21,46 +22,62 @@ const propTypes = PropTypes && {
 };
 
 export class OasisGasPriceWrapper extends PureComponent {
-
   getGasCostEstimate() {
-    const  { currentGasPrice, latestEthereumPrice, transactionGasCostEstimate } = this.props;
+    const {
+      currentGasPrice,
+      latestEthereumPrice,
+      transactionGasCostEstimate
+    } = this.props;
     const currentGasPriceBN = web3.toBigNumber(currentGasPrice);
-    if(transactionGasCostEstimate && latestEthereumPrice) {
-      const cost = web3.fromWei(currentGasPriceBN.mul(transactionGasCostEstimate), ETH_UNIT_ETHER);
+    if (transactionGasCostEstimate && latestEthereumPrice) {
+      const cost = web3.fromWei(
+        currentGasPriceBN.mul(transactionGasCostEstimate),
+        ETH_UNIT_ETHER
+      );
       return (
         <FlexBox className={styles.base}>
-          <div className={styles.gasLabel}>
-            Gas
-          </div>
+          <div className={styles.gasLabel}>Gas</div>
           <div>
-            <span className={styles.detailsTradingFirstCol}>{formatAmount(cost,false, null, 5)} ETH</span>
-            <span className={styles.detailsTradingSecCol} >
-            <span className={styles.estimateUSD}>
-              {formatAmount(cost.mul(latestEthereumPrice.price_usd))} USD
+            <span className={styles.detailsTradingFirstCol}>
+              {formatAmount(cost, false, null, 5)} ETH
             </span>
-          </span>
+            <span className={styles.detailsTradingSecCol}>
+              <span className={styles.estimateUSD}>
+                {formatAmount(cost.mul(latestEthereumPrice.price_usd))} USD
+              </span>
+            </span>
           </div>
         </FlexBox>
-      )
+      );
     }
   }
 
   renderContent() {
     const { gasEstimatePending, gasEstimateError } = this.props;
-    if(gasEstimateError) {
-      return <b>estimate error</b>;
-    }
-    else if(gasEstimatePending) {
-      return <b>estimate pending...</b>;
+    if (gasEstimateError) {
+      return (
+        <InfoBox color="danger" noBorder>
+          <InfoBoxBody>
+            <b>estimate error</b>
+          </InfoBoxBody>
+        </InfoBox>
+      );
+    } else if (gasEstimatePending) {
+      return (
+        <InfoBox noBorder>
+          <InfoBoxBody>
+            <b>estimate pending...</b>
+          </InfoBoxBody>
+        </InfoBox>
+      );
     } else {
       return this.getGasCostEstimate();
     }
-
   }
 
   render() {
     const { className } = this.props;
-    return (<div className={className || ''}>{this.renderContent()}</div>)
+    return <div className={className || ""}>{this.renderContent()}</div>;
   }
 }
 
@@ -68,7 +85,7 @@ export function mapStateToProps(state) {
   return {
     currentGasPrice: transactions.currentGasPriceWei(state),
     latestEthereumPrice: network.latestEthereumPrice(state),
-    latestBlockNumber: network.latestBlockNumber(state),
+    latestBlockNumber: network.latestBlockNumber(state)
   };
 }
 export function mapDispatchToProps(dispatch) {
@@ -77,5 +94,7 @@ export function mapDispatchToProps(dispatch) {
 }
 
 OasisGasPriceWrapper.propTypes = propTypes;
-OasisGasPriceWrapper.displayName = 'OasisGasPrice';
-export default connect(mapStateToProps, mapDispatchToProps)(CSSModules(OasisGasPriceWrapper, styles));
+OasisGasPriceWrapper.displayName = "OasisGasPrice";
+export default connect(mapStateToProps, mapDispatchToProps)(
+  CSSModules(OasisGasPriceWrapper, styles)
+);
