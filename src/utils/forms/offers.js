c0d09e239130b050a2dc32772b7e243d0632d17d
Arkadiusz S <comfort.of.logic@googlemail.com>
2018-05-11 15:53:43 +0200

    Fixed normalization for comma input and multiple leading zeros. (#141)

--- src/utils/forms/offers.js
+++ src/utils/forms/offers.js
@@ -1,31 +1,39 @@
-import web3 from '../../bootstrap/web3';
-
+import web3 from "../../bootstrap/web3";
 
 const normalize = (value, previousValue) => {
-    return parseFloat(value) === 0 ? value :
-      !isFinite(value) ? previousValue: value.replace(/[^\d.-]/g, '').toString();
+  console.log('normalize', value, previousValue);
+  return  ['0',0, ''].includes(value)
+    ? value
+    : !isFinite(value)
+      ? previousValue
+      : value
+          .replace(/,/g, "")
+        .replace(/^0{2,}/, "0")
+          .replace(/[^\d.\\-]/g, "")
+          .replace(/\.{2,}/, "")
+          .toString();
 };
 
-
-const formatValue = (value) =>
-  isFinite(value) ?  web3.toBigNumber(value).toFormat(5): '0';
+const formatValue = value =>
+  isFinite(value)
+    ? web3.toBigNumber(value.replace(/\.$/, "")).toFormat(5)
+    : "0";
 
 const numericFormatValidator = value => {
-  if(!/^(\d+\.?\d*|\.\d+)$/.test(value)) {
+  if (!/^(\d+\.?\d*|\.\d+)$/.test(value)) {
     return `VALIDATOR_ERROR/NOT_NUMERIC_FORMAT`;
   }
 };
 
 const greaterThanZeroValidator = value => {
-  if(!(isFinite(value) && web3.toBigNumber(value).gt(0)) ) {
+  if (!(isFinite(value) && web3.toBigNumber(value).gt(0))) {
     return `VALIDATOR_ERROR/MUST_BE_GREATER_THAN_ZERO`;
   }
 };
-
 
 export {
   normalize,
   formatValue,
   numericFormatValidator,
   greaterThanZeroValidator
-}
+};
