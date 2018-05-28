4183c08ef2536e6f8fdbe82fd15d106f5938534b
Arkadiusz S <comfort.of.logic@googlemail.com>
2018-05-28 12:47:10 +0200

    Fixed OD-110. (#167)

--- src/store/reducers/offers/syncOfferEpic.js
+++ src/store/reducers/offers/syncOfferEpic.js
@@ -1,46 +1,85 @@
-import getOfferTradingPairAndType from '../../../utils/offers/getOfferTradingPairAndType';
-import {getTradingPairOfferCount} from './getTradingPairOffersCount';
-import {OFFER_SYNC_TYPE_INITIAL, OFFER_SYNC_TYPE_UPDATE} from '../offers';
-import {setOfferEpic} from './setOfferEpic';
-import {loadOffer} from './syncOffersEpic';
+import getOfferTradingPairAndType from "../../../utils/offers/getOfferTradingPairAndType";
+import { getTradingPairOfferCount } from "./getTradingPairOffersCount";
+import { OFFER_SYNC_TYPE_INITIAL, OFFER_SYNC_TYPE_UPDATE } from "../offers";
+import { setOfferEpic } from "./setOfferEpic";
+import { loadOffer } from "./syncOffersEpic";
+import { createAction } from "redux-actions";
 
-export const syncOffer = (offerId, syncType = OFFER_SYNC_TYPE_INITIAL, previousOfferState, {
-  doLoadOffer = loadOffer,
-  doGetOfferTradingPairAndType = getOfferTradingPairAndType,
-  doGetTradingPairOfferCount = getTradingPairOfferCount,
-  doSetOfferEpic = setOfferEpic,
-} = {}) => async (dispatch, getState) => {
+const attemtToSyncRemovedOffer = createAction(
+  "OFFERS/ATTEMPT_TO_SYNC_REMOVED_OFFER",
+  syncOfferParams => syncOfferParams
+);
 
+export const syncOffer = (
+  offerId,
+  syncType = OFFER_SYNC_TYPE_INITIAL,
+  previousOfferState,
+  {
+    doLoadOffer = loadOffer,
+    doGetOfferTradingPairAndType = getOfferTradingPairAndType,
+    doGetTradingPairOfferCount = getTradingPairOfferCount,
+    doSetOfferEpic = setOfferEpic
+  } = {}
+) => async (dispatch, getState) => {
   const offer = (await dispatch(doLoadOffer(offerId))).value;
   // const isBuyEnabled = Session.get('isBuyEnabled');
   const [
-    sellHowMuch, sellWhichTokenAddress, buyHowMuch, buyWhichTokenAddress, owner, timestamp,
-  ] = offer;
-
-  const { baseToken, quoteToken, offerType } = doGetOfferTradingPairAndType(
-    { buyWhichTokenAddress, sellWhichTokenAddress, syncType }, getState(),
-  );
-
-  const id = offerId.toString();
-  await dispatch(
-    doGetTradingPairOfferCount(baseToken, quoteToken),
-  );
-
-  dispatch(doSetOfferEpic(Object.assign({
-    id,
     sellHowMuch,
     sellWhichTokenAddress,
     buyHowMuch,
     buyWhichTokenAddress,
     owner,
-    timestamp,
-    offerType,
-    tradingPair: { baseToken, quoteToken },
-    syncType: syncType,
-  }, syncType == OFFER_SYNC_TYPE_UPDATE ? {previousOfferState} : {})));
+    timestamp
+  ] = offer;
 
-  return {
-    offer,
-    offerMeta: { baseToken, quoteToken, offerType },
-  };
+  if (buyWhichTokenAddress && sellWhichTokenAddress) {
+    const { baseToken, quoteToken, offerType } = doGetOfferTradingPairAndType(
+      { buyWhichTokenAddress, sellWhichTokenAddress, syncType },
+      getState()
+    );
+
+    const id = offerId.toString();
+    await dispatch(doGetTradingPairOfferCount(baseToken, quoteToken));
+
+    dispatch(
+      doSetOfferEpic(
+        Object.assign(
+          {
+            id,
+            sellHowMuch,
+            sellWhichTokenAddress,
+            buyHowMuch,
+            buyWhichTokenAddress,
+            owner,
+            timestamp,
+            offerType,
+            tradingPair: { baseToken, quoteToken },
+            syncType: syncType
+          },
+          syncType == OFFER_SYNC_TYPE_UPDATE ? { previousOfferState } : {}
+        )
+      )
+    );
+
+    return {
+      offer,
+      offerMeta: { baseToken, quoteToken, offerType }
+    };
+  } else {
+    dispatch(
+      attemtToSyncRemovedOffer({ offerId, syncType, previousOfferState })
+    );
+    console.log(
+      "trying to sync already removed offer",
+      { offerId, syncType, previousOfferState },
+      {
+        sellHowMuch,
+        sellWhichTokenAddress,
+        buyHowMuch,
+        buyWhichTokenAddress,
+        owner,
+        timestamp
+      }
+    );
+  }
 };
