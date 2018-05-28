e2a5242968c79756d4781fa6abd7e10a80a10a5c
Arkadiusz Szaleniec <arkadiusz.szaleniec@pragmaticcoders.com>
2018-05-28 12:45:49 +0200

    Fixed OD-110.

--- src/utils/offers/getOfferTradingPairAndType.js
+++ src/utils/offers/getOfferTradingPairAndType.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-debugger */
-import { getOfferType } from "../orders";
-import tokens from "../../store/selectors/tokens";
-import getTokenByAddress from "../tokens/getTokenByAddress";
+import { getOfferType } from '../orders';
+import tokens from '../../store/selectors/tokens';
+import getTokenByAddress from '../tokens/getTokenByAddress';
 
 const getOfferTradingPairAndType = (
-  { sellWhichTokenAddress, buyWhichTokenAddress, id, syncType },
-  state,
-  log
+  { sellWhichTokenAddress, buyWhichTokenAddress },
+  state
 ) => {
   const tradingPairs = tokens.tradingPairs(state);
   const offerBuyToken = getTokenByAddress(buyWhichTokenAddress);
@@ -15,36 +14,9 @@ const getOfferTradingPairAndType = (
   const offerTradingPair = tradingPairs.find(tp => {
     const base = tp.get("base");
     const quote = tp.get("quote");
-    const found =
-      (base === offerBuyToken && offerSellToken === quote) ||
+    return (base === offerBuyToken && offerSellToken === quote) ||
       (base === offerSellToken && offerBuyToken === quote);
-    return found;
   });
-
-  if (!offerTradingPair) {
-    // eslint-disable-next-line no-debugger
-    //debugger;
-  }
-  if (!sellWhichTokenAddress) {
-    console.log(sellWhichTokenAddress, buyWhichTokenAddress);
-  }
-
-  if (log || !offerTradingPair) {
-    console.info("Trading pair not found:", {
-      id,
-      syncType,
-      offerType: getOfferType(offerTradingPair.get("base"), {
-        sellWhichTokenAddress,
-        buyWhichTokenAddress,
-        offerBuyToken: getTokenByAddress(buyWhichTokenAddress),
-        offerSellToken: getTokenByAddress(sellWhichTokenAddress)
-      })
-    });
-    if (window.letMeDebugThis) {
-      debugger
-    }
-  }
-
   return {
     baseToken: offerTradingPair.get("base"),
     quoteToken: offerTradingPair.get("quote"),
