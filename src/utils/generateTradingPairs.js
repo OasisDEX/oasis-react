79c2cc3a63de6a890522cc962c21e06807f000e0
Arkadiusz S <comfort.of.logic@googlemail.com>
2018-06-20 21:37:11 +0200

    Remove DGD:DAI, W-GNT:DAI, RHOC:DAI pairs from the trading pairs list. (#214)

--- src/utils/generateTradingPairs.js
+++ src/utils/generateTradingPairs.js
@@ -27,23 +27,11 @@ export const generateTradingPairs = () => {
     },
     {
       base: TOKEN_DIGIX,
-      quote: TOKEN_DAI
-    },
-    {
-      base: TOKEN_DIGIX,
       quote: TOKEN_WRAPPED_ETH
     },
     {
       base: TOKEN_RHOC,
-      quote: TOKEN_DAI
-    },
-    {
-      base: TOKEN_RHOC,
       quote: TOKEN_WRAPPED_ETH
-    },
-    {
-      base: TOKEN_WRAPPED_GNT,
-      quote: TOKEN_DAI
     },
     {
       base: TOKEN_WRAPPED_GNT,
