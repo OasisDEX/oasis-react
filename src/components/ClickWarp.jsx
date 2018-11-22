153835d4c99976d59b022f8383af858e196dc955
Maciek Kamiński <aundumla@gmail.com>
2018-11-22 09:02:24 +0100

    Upgrade announcement

--- src/components/ClickWarp.jsx
+++ src/components/ClickWarp.jsx
@@ -12,7 +12,11 @@ function dismiss() {
 const ClickWarp = () => {
   return (
     <div className={styles.NoEthereumSection}>
-      <h2>Contract upgrade announcement should be here...</h2>
+      <h2>IMPORTANT</h2>
+      <div>We will be moving to a new contract on 12/18/2018.</div>
+      <div>
+        <a href="https://en.reddit.com/r/MakerDAO/comments/9z7h2x/an_announcement_from_the_oasis_team/">Read more here.</a>
+      </div>
       <OasisButton
         // className={tableStyles.inputBtn}
         type="button"
