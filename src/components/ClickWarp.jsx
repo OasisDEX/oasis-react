18cb5f1c7386f58c9beb7974a0bdccce750b5f22
Maciek Kamiński <aundumla@gmail.com>
2018-11-22 09:21:51 +0100

    date format fixed

--- src/components/ClickWarp.jsx
+++ src/components/ClickWarp.jsx
@@ -13,7 +13,7 @@ const ClickWarp = () => {
   return (
     <div className={styles.NoEthereumSection}>
       <h2>IMPORTANT</h2>
-      <div>We will be moving to a new contract on 12/18/2018.</div>
+      <div>We will be moving to a new contract on 18-12-2018.</div>
       <div>
         <a href="https://en.reddit.com/r/MakerDAO/comments/9z7h2x/an_announcement_from_the_oasis_team/">Read more here.</a>
       </div>
