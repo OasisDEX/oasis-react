cd0ee1982cb620fb49344159b931c3fdb3ae2df8
Maciek Kamiński <aundumla@gmail.com>
2018-12-11 10:31:35 +0100

    ClickWarp message updated

--- src/components/ClickWarp.jsx
+++ src/components/ClickWarp.jsx
@@ -13,10 +13,9 @@ const ClickWarp = () => {
   return (
     <div className={styles.NoEthereumSection}>
       <h2>IMPORTANT</h2>
-      <div>We will be moving to a new contract on 18-12-2018.</div>
-      <div>
-        <a href="https://en.reddit.com/r/MakerDAO/comments/9z7h2x/an_announcement_from_the_oasis_team/">Read more here.</a>
-      </div>
+      <div>This is a new contract deployment.</div>
+      <div>Old contract is still <a href="https://old.oasisdex.com">available</a>.</div>
+      <div>Read more <a href="https://en.reddit.com/r/MakerDAO/comments/9z7h2x/an_announcement_from_the_oasis_team/">here.</a></div>
       <OasisButton
         // className={tableStyles.inputBtn}
         type="button"
